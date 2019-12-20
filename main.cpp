3d0eadb327bf0969bf8077af6f476b7f886b0fe2
Christos Vasilakis <christos.vasilakis@arm.com>
2019-12-20 12:25:22 +0000

    replace deprecated methods and deliberately introduce runtime-error

--- main.cpp
+++ main.cpp
@@ -16,12 +16,19 @@
 
 #include "mbed.h"
 
+// Blinking rate in milliseconds
+#define BLINKING_RATE_MS                                                    500
+
 Ticker flipper;
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
  
+ // define the Serial object
+Serial pc(USBTX, USBRX);
+
 void flip() {
     led2 = !led2;
+    pc.printf("Blink! LED2 is now %d\r\n", led2.read());
 }
  
 int main() {
@@ -31,6 +38,7 @@ int main() {
     // spin in a main loop. flipper will interrupt it to call flip
     while(1) {
         led1 = !led1;
-        wait(0.2);
+        pc.printf("Blink! LED1 is now %d\r\n", led1.read());
+        thread_sleep_for(BLINKING_RATE_MS);
     }
 }
